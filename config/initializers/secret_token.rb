c47d8a8858d9685bc1aaceb0b97761939380283d
Dave Woodall <dave@woodalls.me>
2011-10-15 09:11:02 -0600

    working from Mac app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quotesdb::Application.config.secret_token = 'b47b385f910dbe5bd6d1b3cfb7424cd6464b26a4897e2277b2396bea0f03a40a0abb53542941f625cab8619bba2af1134984fb46aea01d827280e052942ac379'
